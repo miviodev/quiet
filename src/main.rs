0179bab93708f100f39e551d6f2ccaa4ba470d63
miviodev <miviodev@gmail.com>
2026-01-16 16:08:29 -0300

    bugfix

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,7 @@
 
 fn main() {
     let arguments: Vec<String> = std::env::args().collect();
-    // println!("{:?}",arguments);
     if arguments.len() >= 2 {
-        //let error: String;
         let command = std::process::Command::new(&arguments[1]).args(&arguments[2..])
         .stdout(std::process::Stdio::null())
         .stderr(std::process::Stdio::piped())
@@ -12,8 +10,18 @@ fn main() {
             eprintln!("[ERROR]: {}", e);
             std::process::exit(1);
         });
-        if !command.status.success() {
-            print!("[ERROR]: {}", String::from_utf8_lossy(&command.stderr));
+        if !command.status.success() { 
+            match command.status.code() {
+                Some(code) =>{
+                        let strr =String::from_utf8_lossy(&command.stderr);
+                        if strr != "" {
+                            eprintln!("[ERROR]: {strr}");
+                        } else {
+                            eprintln!("[ERROR]: Exited with status code: {code}");
+                        }
+               },
+                None => eprintln!("Process terminated by signal")
+            };
         }
     }
 }
